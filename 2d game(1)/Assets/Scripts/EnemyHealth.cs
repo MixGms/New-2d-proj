1ecc4079721cb9371e1ad9b9b669a61b609bef51
MixGms <dryaglin.egor@gmail.com>
2024-02-10 13:09:25 +0200

    second commit

---
+++ 2d game(1)/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    HealthController healthController = new HealthController();
+    float hp;
+
+    private void Awake()
+    {
+        healthController.SetHealth(10);
+        hp = healthController.health;
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        healthController.TakeDamage(dmg);
+        hp = healthController.health;
+        if (hp <= 0) Destroy(gameObject);
+        Debug.Log("Hit");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Enemy health is " + hp.ToString());
+    }
+
+}
---
+++ 2d game(1)/Assets/Scripts/HealthController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public float health;
+
+    public float SetHealth(float hp)
+    {
+        return health = hp;
+    }
+
+    public float TakeDamage(float dmg)
+    {
+        return health -= dmg;
+    }
+}
